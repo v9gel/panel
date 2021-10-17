589d57a6d729c4946b1669d6b29ab3057cba82b1
vyacheslav.nagel <v9gel@mail.ru>
2021-10-17 16:21:15 +0700

    + Stars

---
+++ src/components/ui/Stars.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps, toRefs } from "vue";
+const props = defineProps({ count: Number, large: Boolean });
+const { count, large } = toRefs(props);
+</script>
+
+<template>
+  <div>
+    <img
+      v-for="index in count"
+      :key="index"
+      class="star"
+      :src="large ? 'images/star_30.png' : 'images/star_22.png'"
+    />
+  </div>
+</template>
+
+<style scoped lang="less">
+.star {
+  width: fit-content;
+  height: fit-content;
+}
+.star + .star {
+  margin-left: 4px;
+}
+</style>
